10da67ff27a9bb3b01a49df50683cd50476b241e
Leonardo2397 <leonardoferrante753@gmail.com>
2025-11-23 17:51:23 +0100

    edit

--- src/services/bookingService.js
+++ src/services/bookingService.js
@@ -3,8 +3,13 @@ import axios from "axios";
 const API_URL = "http://localhost:8080/api/bookings";
 
 export const getUserBookings = async () => {
-  const res = await axios.get(`${API_URL}/user/me`, {
-    withCredentials: true,
-  });
-  return res.data;
+  try {
+    const res = await axios.get(`${API_URL}/user/me`, {
+      withCredentials: true, // fondamentale per inviare il cookie JWT
+    });
+    return res.data;
+  } catch (error) {
+    console.error("Errore fetch prenotazioni:", error);
+    throw error; // rilancia per gestirlo nel componente React
+  }
 };
